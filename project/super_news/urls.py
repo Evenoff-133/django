20a3c91dfd28c21cfafaf7e9e26292e72f08712e
Matthew Ivanov <matthewblacktool@gmail.com>
2021-12-05 23:53:07 +0200

    polls/my templates/

--- project/super_news/urls.py
+++ project/super_news/urls.py
@@ -16,12 +16,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+from django.conf import settings
+from django.conf.urls.static import static
 
 from polls.views import index, detail, blog_heandler
 
 urlpatterns = [
     path('', index ),
-    path('polls/<int:question_id>', detail),
+    path('polls/<int:question_id>/', detail),
     path('blog/', blog_heandler),
     path('admin/', admin.site.urls),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
