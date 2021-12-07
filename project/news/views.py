d1d2ea0b73467b7602fbcb58bf81df97e88a4a7a
Matthew Ivanov <matthewblacktool@gmail.com>
2021-12-07 20:30:35 +0200

    change html5 temlplate

---
+++ project/news/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+
+def blog_heandler(request):
+    context = {}
+    return render(request, 'news/blog.html', context)
+
+
+def blog_heandler(request):
+    context = {}
+    return render(request, 'news/blog-single.html', context)
+
+
+def blog_heandler(request):
+    context = {}
+    return render(request, 'news/blog-single.html', context)
+
+
+def blog_heandler(request):
+    context = {}
+    return render(request, 'news/blog-single.html', context)
+
+
+def blog_heandler(request):
+    context = {}
+    return render(request, 'news/blog-single.html', context)
