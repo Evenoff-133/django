b88604d734b5eaaf4b4f9d9a1d300b751b01ea52
matt <eeshowivanov@gmail.com>
2022-01-02 18:29:30 +0200

    debug

--- web_app/super_news/settings.py
+++ web_app/super_news/settings.py
@@ -85,7 +85,7 @@ WSGI_APPLICATION = 'super_news.wsgi.application'
 DATABASES = {
      'default': {
          'ENGINE': 'django.db.backends.sqlite3',
-         'NAME': BASE_DIR / 'db.sqlite3',
+         'NAME': str(os.path.join(BASE_DIR, "db.sqlite3")),
      }
  }
 
@@ -142,4 +142,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-from .settings_local import *
+# from .settings_local import *
--- web_app/super_news/urls.py
+++ web_app/super_news/urls.py
@@ -5,7 +5,6 @@ The `urlpatterns` list routes URLs to views. For more information please see:
 from django.contrib import admin
 from django.urls import include, path
 import debug_toolbar
-
 from django.conf import settings
 from django.conf.urls.static import static
 
